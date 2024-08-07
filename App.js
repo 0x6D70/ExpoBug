c5266c58515bdb4c7650a9a7ee58825955b128cc
Markus Pfeifenberger <pfeifenberger.markus2003@gmail.com>
2024-08-07 22:06:31 +0200

    add example with bug

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { Camera, CameraView } from 'expo-camera';
 
 export default function App() {
+  let camera = useRef(null)
+  const [hasPermission, setHasPermission] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const takePicture = async () => {
+    console.log('taking picture ...')
+
+    const picture = await camera.current.takePictureAsync({
+      exif: true,
+      base64: true,
+      skipProcessing: false,
+    })
+
+    console.log(`height: ${picture.height}, width: ${picture.width}`)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CameraView
+      ref={camera}
+      style={{ width: "100%", height: "100%" }}
+      ratio='16:9'
+      onCameraReady={console.log}
+      onMountError={console.warn}
+    >
+      <TouchableOpacity
+        style={{
+          position: 'relative',
+          width: 120,
+          height: 120,
+          backgroundColor: 'white',
+          margin: 'auto'
+        }}
+        onPress={takePicture}
+      >
+        <Text>take picture</Text>
+      </TouchableOpacity>
+    </CameraView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
